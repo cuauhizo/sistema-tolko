2b863ce4b8722edb27e73566f2cf1fb905325395
cuauhizo <cuauhizo@gmail.com>
2026-02-13 13:21:11 -0600

    cambio base

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,14 @@
-import { createPool } from 'mysql2/promise';
-import { config } from 'dotenv';
+import { createPool } from 'mysql2/promise'
+import { config } from 'dotenv'
 
-config();
+config()
 
 export const pool = createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'bd_sistema_tolko',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'bdsistema-tolko',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+})
